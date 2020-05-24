7a934d619b939c618b356c5a14cbc7cdbf3b015d
uki00a <uki00a@gmail.com>
2020-05-24 19:33:10 +0900

    chore: add examples

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# Carol (WIP)
+# Carol
+
+Note: This module is **still work in progress**.
 
 This module is based on excellent works of [lorca](https://github.com/zserge/lorca) and [carlo](https://github.com/GoogleChromeLabs/carlo).
 
--- examples/hello.ts
+++ examples/hello.ts
@@ -1,4 +1,24 @@
 import { launch } from "../mod.ts";
+import { serve } from "https://deno.land/std@0.51.0/http/server.ts";
+import {
+  join,
+  fromFileUrl,
+  dirname,
+} from "https://deno.land/std@0.51.0/path/mod.ts";
+
+const address = "127.0.0.1:8000";
+const file = await Deno.readFile(
+  fromFileUrl(join(dirname(import.meta.url), "index.html")),
+);
+
+(async () => {
+  for await (const req of serve(address)) {
+    req.respond({
+      headers: new Headers([["Content-Type", "text/html"]]),
+      body: file,
+    });
+  }
+})();
 
 const app = await launch({
   title: "Hello carol",
@@ -6,4 +26,5 @@ const app = await launch({
   height: 320,
 });
 
-await app.load("data:text/html,<div>Hello, world!</div>");
+await app.exposeFunction("greet", (name: string) => `Hello, ${name}!`);
+await app.load(`http://${address}`);
