043b3e620b8a4b7a33f38ac7764d37c74832d55c
uki00a <uki00a@gmail.com>
2020-05-31 16:44:30 +0900

    fix: disable debug logging by default (#26)

---
+++ logger.ts
@@ -0,0 +1,22 @@
+export interface Logger {
+  log(message: any, ...args: any[]): void;
+  error(message: any, ...args: any[]): void;
+  debug(message: any, ...args: any[]): void;
+}
+
+const noop = () => {};
+const NullLogger = {
+  log: noop,
+  error: noop,
+  debug: noop,
+} as Logger;
+
+export function createLogger(env = Deno.env): Logger {
+  const debug = env.get("CAROL_DEBUG");
+  if (debug === "1" || debug === "true") {
+    // TODO improve logging
+    return console;
+  } else {
+    return NullLogger;
+  }
+}
