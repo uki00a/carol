0f3203f2380ab84b1f16e23f19627ddb19c28381
uki00a <uki00a@gmail.com>
2020-07-22 07:33:44 +0900

    chore: pass "deno lint" (#39)

--- application.ts
+++ application.ts
@@ -2,9 +2,14 @@ import { Chrome } from "./chrome.ts";
 
 export interface Application {
   load(url: string): Promise<void>;
+  // deno-lint-ignore no-explicit-any
   evaluate(expression: string): Promise<any>;
   exit(): Promise<void>;
-  exposeFunction(name: string, binding: (...args: any[]) => any): Promise<void>;
+  exposeFunction(
+    name: string,
+    // deno-lint-ignore no-explicit-any
+    binding: (...args: any[]) => any,
+  ): Promise<void>;
   serveFolder(folder: string, prefix?: string): void;
   serveOrigin(base: string, prefix?: string): void;
   onExit(): Promise<void>;
@@ -12,22 +17,22 @@ export interface Application {
 
 export function createApplication(chrome: Chrome): Application {
   return {
-    load(url: string): Promise<void> {
+    load(url) {
       return chrome.load(url);
     },
-    evaluate(expression: string): Promise<any> {
+    evaluate(expression) {
       return chrome.evaluate(expression);
     },
     async exit() {
       await chrome.exit();
     },
-    exposeFunction(name: string, binding: (...args: any[]) => any) {
+    exposeFunction(name, binding) {
       return chrome.bind(name, (args) => binding(...args));
     },
-    serveFolder(folder: string, prefix?: string) {
+    serveFolder(folder, prefix) {
       return chrome.serveFolder(folder, prefix);
     },
-    serveOrigin(base: string, prefix?: string) {
+    serveOrigin(base, prefix) {
       return chrome.serveOrigin(base, prefix);
     },
     onExit() {
