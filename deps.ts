50c53903cd8a3bf243677fcbf50b518cbfccc8d3
uki00a <uki00a@gmail.com>
2020-08-23 12:33:09 +0900

    upgrade: bump deno to 1.3.1 and std to v0.66.0

--- deps.ts
+++ deps.ts
@@ -1,22 +1,22 @@
-export { exists } from "https://deno.land/std@v0.64.0/fs/exists.ts";
-export { dirname, join } from "https://deno.land/std@v0.64.0/path/mod.ts";
-export { BufReader } from "https://deno.land/std@v0.64.0/io/bufio.ts";
-export { concat } from "https://deno.land/std@v0.64.0/bytes/mod.ts";
-export { decode, encode } from "https://deno.land/std@v0.64.0/encoding/utf8.ts";
-export { encode as encodeToBase64 } from "https://deno.land/std@v0.64.0/encoding/base64.ts";
+export { exists } from "https://deno.land/std@0.66.0/fs/exists.ts";
+export { dirname, join } from "https://deno.land/std@0.66.0/path/mod.ts";
+export { BufReader } from "https://deno.land/std@0.66.0/io/bufio.ts";
+export { concat } from "https://deno.land/std@0.66.0/bytes/mod.ts";
+export { decode, encode } from "https://deno.land/std@0.66.0/encoding/utf8.ts";
+export { encode as encodeToBase64 } from "https://deno.land/std@0.66.0/encoding/base64.ts";
 export {
   deferred,
   Deferred,
-} from "https://deno.land/std@v0.64.0/async/deferred.ts";
+} from "https://deno.land/std@0.66.0/async/deferred.ts";
 export {
   connectWebSocket,
   isWebSocketCloseEvent,
-} from "https://deno.land/std@v0.64.0/ws/mod.ts";
-export { sprintf } from "https://deno.land/std@v0.64.0/fmt/printf.ts";
+} from "https://deno.land/std@0.66.0/ws/mod.ts";
+export { sprintf } from "https://deno.land/std@0.66.0/fmt/printf.ts";
 export {
   assert,
   assertEquals,
   assertStrictEquals,
   assertThrowsAsync,
   fail,
-} from "https://deno.land/std@v0.64.0/testing/asserts.ts";
+} from "https://deno.land/std@0.66.0/testing/asserts.ts";
