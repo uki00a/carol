6f5a2e362e645bab03362156dd8bd60362887984
rado_konuch <reminqo88@gmail.com>
2021-08-25 14:28:12 +0200

    Updated deno version to 1.13.2 and puppeteer version to 10.2.0

--- deps.ts
+++ deps.ts
@@ -1,22 +1,22 @@
-export { exists } from "https://deno.land/std@0.97.0/fs/exists.ts";
+export { exists } from "https://deno.land/std@0.106.0/fs/exists.ts";
 export {
   dirname,
   fromFileUrl,
   join,
   resolve,
-} from "https://deno.land/std@0.97.0/path/mod.ts";
-export { BufReader } from "https://deno.land/std@0.97.0/io/bufio.ts";
-export { concat } from "https://deno.land/std@0.97.0/bytes/mod.ts";
+} from "https://deno.land/std@0.106.0/path/mod.ts";
+export { BufReader } from "https://deno.land/std@0.106.0/io/bufio.ts";
+export { concat } from "https://deno.land/std@0.106.0/bytes/mod.ts";
 // TODO: Remove this import statement.
 export { decode, encode } from "https://deno.land/std@0.84.0/encoding/utf8.ts";
 export {
   decode as decodeFromBase64,
   encode as encodeToBase64,
-} from "https://deno.land/std@0.97.0/encoding/base64.ts";
-export { deferred } from "https://deno.land/std@0.97.0/async/deferred.ts";
-export type { Deferred } from "https://deno.land/std@0.97.0/async/deferred.ts";
+} from "https://deno.land/std@0.106.0/encoding/base64.ts";
+export { deferred } from "https://deno.land/std@0.106.0/async/deferred.ts";
+export type { Deferred } from "https://deno.land/std@0.106.0/async/deferred.ts";
 
-export { readAll, writeAll } from "https://deno.land/std@0.97.0/io/util.ts";
+export { readAll, writeAll } from "https://deno.land/std@0.106.0/io/util.ts";
 
 export {
   assert,
@@ -27,11 +27,10 @@ export {
   fail,
 } from "https://deno.land/std@0.95.0/testing/asserts.ts";
 
-// @deno-types="https://unpkg.com/puppeteer@5.5.0/lib/esm/puppeteer/web.d.ts"
-export { default as puppeteer } from "https://unpkg.com/puppeteer@5.5.0/lib/esm/puppeteer/web.js";
-export { EventEmitter } from "https://unpkg.com/puppeteer@5.5.0/lib/esm/puppeteer/common/EventEmitter.js";
+export { default as puppeteer } from "https://unpkg.com/puppeteer@10.2.0/lib/esm/puppeteer/web.js";
+export { EventEmitter } from "https://unpkg.com/puppeteer@10.2.0/lib/esm/puppeteer/common/EventEmitter.js";
 
-export type { Browser } from "https://unpkg.com/puppeteer@5.5.0/lib/esm/puppeteer/common/Browser.js";
-export type { Target } from "https://unpkg.com/puppeteer@5.5.0/lib/esm/puppeteer/common/Target.js";
-export type { CDPSession } from "https://unpkg.com/puppeteer@5.5.0/lib/esm/puppeteer/common/Connection.js";
-export type { Page } from "https://unpkg.com/puppeteer@5.5.0/lib/esm/puppeteer/common/Page.js";
+export type { Browser } from "https://unpkg.com/puppeteer@10.2.0/lib/esm/puppeteer/common/Browser.js";
+export type { Target } from "https://unpkg.com/puppeteer@10.2.0/lib/esm/puppeteer/common/Target.js";
+export type { CDPSession } from "https://unpkg.com/puppeteer@10.2.0/lib/esm/puppeteer/common/Connection.js";
+export type { Page } from "https://unpkg.com/puppeteer@10.2.0/lib/esm/puppeteer/common/Page.js";
