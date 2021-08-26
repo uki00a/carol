064f596d63f1ea36e6ade4de5d4cdc0e36704c87
rado_konuch <reminqo88@gmail.com>
2021-08-26 19:42:04 +0200

    Fixed flaky tests.

--- test_deps.ts
+++ test_deps.ts
@@ -1,3 +1,2 @@
 export { serve } from "https://deno.land/std@0.106.0/http/mod.ts";
 export type { Server } from "https://deno.land/std@0.106.0/http/mod.ts";
-export { serveFile } from "https://deno.land/std@0.106.0/http/file_server.ts";
