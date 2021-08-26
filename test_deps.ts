569f923896238a88f21e67f41c24f9d1acae8f8b
uki00a <uki00a@gmail.com>
2021-08-26 22:15:24 +0900

    chore: Try to bump deno to v1.13.2

--- test_deps.ts
+++ test_deps.ts
@@ -1,3 +1,3 @@
-export { serve } from "https://deno.land/std@0.97.0/http/mod.ts";
-export type { Server } from "https://deno.land/std@0.97.0/http/mod.ts";
-export { serveFile } from "https://deno.land/std@0.97.0/http/file_server.ts";
+export { serve } from "https://deno.land/std@0.106.0/http/mod.ts";
+export type { Server } from "https://deno.land/std@0.106.0/http/mod.ts";
+export { serveFile } from "https://deno.land/std@0.106.0/http/file_server.ts";
