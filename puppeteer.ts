3e36d471a8ed14e9ee3e4012de3eb9624a7a278d
uki00a <uki00a@gmail.com>
2021-09-23 18:30:03 +0900

    fix: compilation error

--- puppeteer.ts
+++ puppeteer.ts
@@ -16,8 +16,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { BufReader, decode, puppeteer, resolve } from "./deps.ts";
-import type { Browser, BrowserWebSocketTransport, Target } from "./deps.ts";
+import {
+  BrowserWebSocketTransport,
+  BufReader,
+  decode,
+  puppeteer,
+  resolve,
+} from "./deps.ts";
+import type { Browser, Target } from "./deps.ts";
 import type { AppOptions } from "./types.ts";
 import { getLocalDataDir } from "./util.ts";
 
