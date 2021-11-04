b835539e901bce90587824b29b70f92fdb306dd0
rado_konuch <reminqo88@gmail.com>
2021-11-04 07:56:28 +0100

    Fixed type linting errors

--- types.ts
+++ types.ts
@@ -70,12 +70,12 @@ export interface Application {
   /**
    * Close the app windows.
    */
-  exit(message?: any): Promise<void>;
+  exit(message?: string): Promise<string|void>;
 
   /**
    * Returns the promise that will be resolved when the app is closed.
    */
-  onExit(): Promise<any>;
+  onExit(): Promise<string|void>;
 
   /**
    * @return main window.
