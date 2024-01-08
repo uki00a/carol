5193dc0b32446d1bb02999176f0b7cb282c45511
uki00a <uki00a@gmail.com>
2024-01-08 15:20:21 +0900

    chore: bump deno and std

--- deps.ts
+++ deps.ts
@@ -1,22 +1,21 @@
-export { exists } from "https://deno.land/std@0.149.0/fs/exists.ts";
-export {
-  dirname,
-  fromFileUrl,
-  join,
-  resolve,
-} from "https://deno.land/std@0.149.0/path/mod.ts";
-export { BufReader } from "https://deno.land/std@0.149.0/io/bufio.ts";
-export { concat } from "https://deno.land/std@0.149.0/bytes/mod.ts";
+export { exists } from "https://deno.land/std@0.211.0/fs/exists.ts";
+export { dirname } from "https://deno.land/std@0.211.0/path/dirname.ts";
+export { fromFileUrl } from "https://deno.land/std@0.211.0/path/from_file_url.ts";
+export { join } from "https://deno.land/std@0.211.0/path/join.ts";
+export { resolve } from "https://deno.land/std@0.211.0/path/resolve.ts";
+export { BufReader } from "https://deno.land/std@0.211.0/io/bufio.ts";
+export { concat } from "https://deno.land/std@0.211.0/bytes/mod.ts";
 // TODO: Remove this import statement.
 export { decode, encode } from "https://deno.land/std@0.84.0/encoding/utf8.ts";
 export {
-  decode as decodeFromBase64,
-  encode as encodeToBase64,
-} from "https://deno.land/std@0.149.0/encoding/base64.ts";
+  decodeBase64 as decodeFromBase64,
+  encodeBase64 as encodeToBase64,
+} from "https://deno.land/std@0.211.0/encoding/base64.ts";
 export { deferred } from "https://deno.land/std@0.149.0/async/deferred.ts";
 export type { Deferred } from "https://deno.land/std@0.149.0/async/deferred.ts";
 
-export { readAll, writeAll } from "https://deno.land/std@0.149.0/io/util.ts";
+export { readAll } from "https://deno.land/std@0.211.0/streams/read_all.ts";
+export { writeAll } from "https://deno.land/std@0.211.0/streams/write_all.ts";
 
 export {
   assert,
@@ -25,7 +24,7 @@ export {
   assertStrictEquals,
   assertStringIncludes,
   fail,
-} from "https://deno.land/std@0.149.0/testing/asserts.ts";
+} from "https://deno.land/std@0.149.0/assert/mod.ts";
 
 export { default as puppeteer } from "https://unpkg.com/puppeteer@13.3.2/lib/esm/puppeteer/web.js";
 export { EventEmitter } from "https://unpkg.com/puppeteer@13.3.2/lib/esm/puppeteer/common/EventEmitter.js";
